65231c368da64499d6203b35aefd83524702f1b5
Jameson Campbell <jcampbell@flexterapp.com>
2019-03-18 16:27:50 -0500

    page styling and random index generated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // hwk hint: splice() method
 
 // Starting shows array
-var show = ["Bloodline", "Billions", "Westworld", "Veep", "Deadwood"];
+var show = ["bloodline", "billions", "westworld", "veep", "deadwood"];
 
 const maxTries = 10;            // Maximum number of tries player has
 
@@ -12,3 +12,21 @@ var remainingGuesses = 0;       // How many tries the player has left
 var gameStarted = false;        // Flag to tell if the game has started
 var hasFinished = false;        // Flag for 'press any key to try again'     
 var wins = 0;                   // How many wins has the player racked up
+
+// creates a random selection from show variable
+var randomShow = show[Math.floor(Math.random() * show.length)];
+
+// set up answer array
+var answerArray = [];
+for (var i = 0; i < randomShow.length; i++) {
+    answerArray[i] = "_";
+}
+
+
+/* var el = document.getElementById("currentWord");
+    el.innerHTML = answerArray;
+    el.innerHTML = answerArray.join(" ");
+
+var la = document.getElementById("totalWins");
+    la.innerHTML = wins;
+*/
