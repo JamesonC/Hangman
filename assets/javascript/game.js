21f10ca62ec2eec99b1cf23dfc7136a8e1505f30
Jameson Campbell <jcampbell@flexterapp.com>
2019-03-18 21:20:50 -0500

    displays initial values when game is started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,60 @@
 // Starting shows array
 var word = ["bloodline", "billions", "westworld", "veep", "deadwood"];
 
-var guessedLetters = []; // Stores the letters the user guessed
-var currentWordIndex; // Index of the current word in the array
-var guessingWord = []; // Word we build to match the current word
-var remainingGuesses = 0; // How many tries the player has left
-var gameStarted = true; // Flag to tell if the game has started
-var hasFinished = false; // Flag for 'press any key to try again'     
-var wins = 0; // How many wins has the player racked up
-var maxGuesses = 10; // Max number of guesses a user receives
+var guessedLetters = [];        // Stores the letters the user guessed
+var indexOfWord;                // Indexed element of the current word in the array
+var guessingWord = [];          // Word we build to match the current word
+var remainingGuesses = 0;       // How many tries the player has left  
+var maxGuesses = 10;            // Max number of guesses a user receives
+var wins = 0;                   // How many wins has the player racked up
+var gameStarted = false;        // Flag to tell if the game has started
+var hasFinished = false;        // Flag for 'press any key to try again'   
 
-// creates a random selection from show variable
-var randomWord = word[Math.floor(Math.random() * word.length)];
 
-// set up answer array
-for (var i = 0; i < randomWord.length; i++) {
+// displays "wins" at 0 
+var totalWins = document.getElementById("totalWins");
+totalWins.innerHTML = wins;
+
+
+// finds a random indexed word element from 'word' array
+indexOfWord = word[Math.floor(Math.random() * word.length)];
+
+
+// displays exact number of "_"'s for indexOfWord
+for (var i = 0; i < indexOfWord.length; i++) {
     console.log(guessingWord[i] = "_");
 }
 
-// makes underscores beneath "Current Word"
-var underScores = document.getElementById("currentWord");
-underScores.innerHTML = guessingWord;
-underScores.innerHTML = guessingWord.join(" ");
+
+// makes underscores for guessingWord characters
+var currentWordCharacters = document.getElementById("currentWord");
+currentWordCharacters.innerHTML = guessingWord.join(" ");
 
 
-if (gameStarted) {
+// displays remaining guesses
+if (!gameStarted) {
     var remainingGuesses = document.getElementById("remainingGuesses");
     remainingGuesses.innerHTML = maxGuesses;
 }
 
 
 // function that listens for user input 
-/* document.onkeyup = function(event) {
-    var userChoice = event.key;
-} */
+document.onkeyup = function (event) {
+    console.log(userChoice = event.key);
+}
 
-/*
 
-var la = document.getElementById("totalWins");
-    la.innerHTML = wins;
-*/
+// function that resets the game after a win or loss
+function resetGame() {
+    remainingGuesses = maxGuesses;
+    gameStarted = true;
+
+    indexOfWord = word[Math.floor(Math.random() * word.length)];
+
+    guessedLetters = [];
+    guessingWord = [];
+
+    for (var i = 0; i < word[indexOfWord.length]; i++) {
+        guessingWord.push("_");
+    }
+}
