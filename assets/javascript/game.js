c749060f8b49fc6a44d49931c0d79b4299a79b30
Jameson Campbell <jcampbell@flexterapp.com>
2019-03-22 12:09:58 -0500

    remaining guessesresets back to max afer user wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,6 @@ var guessingWord = []; // Word we build to match the current word
 var remainingGuesses = 0; // How many tries the player has left  
 var maxGuesses = 10; // Max number of guesses a user receives
 var wins = 0; // How many wins has the player racked up
-var hasFinished = false; // Flag for 'press any key to try again'   
 var incorrectLetters = [] // Stores incorrect letters
 
 
@@ -52,9 +51,10 @@ document.onkeyup = function (event) {
 // executes user's letter guess
 function evaluateGuess(key) {
 
-    if (currentWord.indexOf(key) === -1 && maxGuesses >= 0) {
+    if (currentWord.indexOf(key) === -1 && maxGuesses > 0) {
 
-        remainingGuesses.innerHTML = (maxGuesses--);
+        maxGuesses--
+        remainingGuesses.innerHTML = maxGuesses;
 
         incorrectLetters.push(key) // captures key pressed and pushes key into incorrectLetters array
         guessedLetters.innerHTML = incorrectLetters;
@@ -63,17 +63,19 @@ function evaluateGuess(key) {
         for (var i = 0; i < currentWord.length; i++) {
             if (key === currentWord[i]) {
                 dashes[i] = key;
-            } 
-        } 
+            }
+        }
         wordCharacters.innerHTML = dashes.join(" ");
-    } checkWin()
+    }
+
+    checkWin()
 };
 
 
 function checkWin() {
     console.log(dashes.indexOf("_") === -1)
     if (dashes.indexOf("_") === -1) {
-        console.log("it got through")
+        console.log("Check Win Function")
         wins++;
         totalWins.innerHTML = wins;
         resetGame();
@@ -83,14 +85,14 @@ function checkWin() {
 
 // function that resets the game after a win or loss
 function resetGame() {
-    console.log("in reset function")
-    remainingGuesses = maxGuesses;
+    console.log("In Reset Function")
     currentWord = word[Math.floor(Math.random() * word.length)];
     incorrectLetters = [];
     guessedLetters = [];
     guessingWord = [];
     dashes = [];
-    
+    maxGuesses = 10;
+
     for (var i = 0; i < currentWord.length; i++) {
         dashes.push("_");
     }
@@ -98,10 +100,7 @@ function resetGame() {
     var wordCharacters = document.getElementById("currentWord");
     wordCharacters.innerHTML = dashes.join(" ");
 
-    // displays "Guesses Remaining" at 10
     var remainingGuesses = document.getElementById("remainingGuesses");
     remainingGuesses.innerHTML = maxGuesses;
 
-    // function that listens/displays user key input 
-    var guessedLetters = document.getElementById("guessedLetters");
 };
